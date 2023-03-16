02121404b0bd64184b00c6084385eb00d45373f5
Ricardo Dias <ricardodias.dev@gmail.com>
2023-03-16 20:44:15 -0300

    Apagando tarefa

--- App.js
+++ App.js
@@ -41,7 +41,16 @@ export default function App() {
   }, [user]);
 
   function handleDelete(key) {
-    alert(key);
+    firebase
+      .database()
+      .ref("tarefas")
+      .child(user)
+      .child(key)
+      .remove()
+      .then(() => {
+        const findTasks = tasks.filter((item) => item.key !== key);
+        setTasks(findTasks);
+      });
   }
 
   function handleEdit(data) {
